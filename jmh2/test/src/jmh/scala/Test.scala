e2bd9992e300944b9440fd3b65db73ff68d9de94
Sudarshan S <ssarolkar@box.com>
2020-09-22 14:06:26 -0700

    WIP

---
+++ jmh2/test/src/jmh/scala/Test.scala
@@ -0,0 +1,15 @@
+package t1;
+
+import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.runner.Runner;
+import org.openjdk.jmh.runner.RunnerException;
+import org.openjdk.jmh.runner.options.Options;
+import org.openjdk.jmh.runner.options.OptionsBuilder;
+
+class Test {
+
+    @Benchmark
+    def scalaJmh() {
+        // this method was intentionally left blank.
+    }
+}
